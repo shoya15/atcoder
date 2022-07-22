71e430898b8fb1e7daf2e9cca5208af20a41d694
yugao-sh <s.h.rainight@gmail.com>
2022-07-22 22:22:17 +0900

    try

---
+++ C++/A/4.cpp
@@ -0,0 +1,13 @@
+/*
+高橋君は N 円借金をしました。
+倍返しでおもてなしするのが流行だと小耳に挟んだ高橋君は、2 * N 円返済します。
+高橋君が返済する金額 2 * N 円を出力してください。
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    cout << 2 * N << endl;
+}
---
+++ C++/A/7.cpp
@@ -0,0 +1,11 @@
+/*
+一直線に並んでいる木々が n 本あるという情報が与えられるので、隣り合う木の間の数を出力するプログラムをたかはし君のために作ってあげてください。
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n - 1 << endl;
+}
