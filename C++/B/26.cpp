f708eab12bac6107905de2a1b63eb4f3a0516158
yugao-sh <s.h.rainight@gmail.com>
2022-08-21 22:38:25 +0900

    0821

--- C++/B/26.cpp
+++ C++/B/26.cpp
@@ -1,6 +1,25 @@
 /*
-
+高橋君は、丸が大好きです。今日も、原点を中心とした大きさの違う円を N 個書きました。
+その円の集合に対し、外側から赤白交互に色を塗ったとき、赤く塗られる部分の面積を出力しなさい。
 */
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main() {}
+int main()
+{
+    int n, sum = 0;
+    cin >> n;
+    vector<int> r(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> r[i];
+        r[i] *= r[i];
+    }
+    sort(r.begin(), r.end(), greater<int>());
+    for (int j = 0; j < n; j++)
+    {
+        if (j % 2 != 0)
+            r[j] = -r[j];
+        sum += r[j];
+    }
+    cout << fixed << setprecision(10) << sum * M_PI << endl;
+}
--- C++/B/29.cpp
+++ C++/B/29.cpp
@@ -1,6 +1,24 @@
 /*
-
+英小文字からなる 12 個の文字列 S1, S2, ..., S12が入力されます。
+これらの文字列のうち、文字 r が含まれるものの個数を数えてください。
 */
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main() {}
+int main()
+{
+    int sum = 0;
+    vector<string> s(12);
+    for (int i = 0; i < 12; i++)
+    {
+        cin >> s[i];
+        for (int j = 0; j < s[i].size(); j++)
+        {
+            if (s[i][j] == 'r')
+            {
+                sum++;
+                break;
+            }
+        }
+    }
+    cout << sum << endl;
+}
