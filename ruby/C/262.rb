899623e8a19f8dca37045eedf18008ce65f45b4a
shoya15 <s.h.rainight@gmail.com>
2022-11-25 23:13:25 +0900

    22/11/25

--- ruby/C/262.rb
+++ ruby/C/262.rb
@@ -1,3 +1,15 @@
-n=gets.split.map(&:to_i)
-a= gets.split.map(&:to_i)
-ans=0
+n = gets.to_i
+a = gets.split.map(&:to_i)
+
+cnt = 0
+a.each_with_index do|i, j|
+    cnt += 1 if i == j + 1
+end
+
+# a[i] = iの総数cnt、ここから2つ取ってくるcntC2
+ans = (cnt * (cnt - 1)) / 2
+for i in 0..n - 1
+    # max: a[i], min: a[j](=i)だから a[i]>i
+    ans += 1 if a[i] > i + 1 && a[a[i] - 1] == i + 1
+end
+puts ans
