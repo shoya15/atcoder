1cb4705c994d9628c4e3dd83dc408c52c502b616
shoya15 <s.h.rainight@gmail.com>
2023-01-28 23:18:38 +0900

    23/01/28

--- ruby/C/287-d.rb
+++ ruby/C/287-d.rb
@@ -1,28 +1,26 @@
 s = gets.chomp.chars
 t = gets.chomp.chars
-l = t.size
-(l + 1).times do|x|
-    i = 0
-    while i < x
-        if s[i] == "?" || t[i] == "?"
-            i += 1
-            next
-        end
-        if s[i] != t[i]
-            puts "No"
-            break
-        end
+lt = t.size
+
+pre = 0 # 先頭何文字までが一致しているか
+lt.times do|i|
+    break if s[i] != "?" && t[i] != "?" && s[i] != t[i]
+    pre += 1
+end
+
+s = s.reverse
+t = t.reverse
+
+suf = 0 # 末尾何文字までが一致しているか
+lt.times do|i|
+    break if s[i] != "?" && t[i] != "?" && s[i] != t[i]
+    suf += 1
+end
+
+(lt + 1).times do|i|
+    if pre >= i && suf >= lt - i
+        puts "Yes"
+    else
+        puts "No"
     end
-    j = l - x
-    while j > 0
-        if s[-j] == "?" || t[-j] == "?"
-            j -= 1
-            next
-        end
-        if s[-j] != t[-j]
-            puts "No"
-            break
-        end
-    end
-    puts "Yes" if i == x && j == 0
 end
