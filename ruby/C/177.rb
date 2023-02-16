a65701c2fbbd35717c541ef40f148e338fa49b45
shoya15 <s.h.rainight@gmail.com>
2023-02-16 15:50:09 +0900

    23/02/16

--- ruby/C/177.rb
+++ ruby/C/177.rb
@@ -1,2 +1,15 @@
-d, s, t = gets.split.map(&:to_i)
-puts s * t >= d ? "Yes" : "No"
+n = gets.to_i
+a = gets.split.map(&:to_i)
+arr = Array.new(n + 1, 0)
+n.times do|i|
+    arr[i + 1] = arr[i] + a[i]
+end
+
+mod = 1000000007
+ans = 0
+n.times do|i|
+    sum = (arr[n] - arr[i + 1]) % mod
+    ans += a[i] * sum
+    ans %= mod
+end
+puts ans
