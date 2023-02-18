612ff3813c458d9d164e10e86e81a0fe514f6b38
廣瀬翔也 <hiroseshouya@hiroseshouyanoMacBook-Pro.local>
2023-02-19 05:01:37 +0900

    23/02/19

--- ruby/C/173.rb
+++ ruby/C/173.rb
@@ -1,2 +1,20 @@
-n = gets.to_i % 1000
-p n == 0 ? 0 : 1000 - n
+h, w, k = gets.split.map(&:to_i)
+c = []
+h.times do
+    c << gets.split("")
+end
+
+ans = 0
+# 0のとき選ばれない(赤でない)
+[0, 1].repeated_permutation(h).each do|i|
+    [0, 1].repeated_permutation(w).each do|j|
+        cnt = 0
+        i.each_with_index do|ii, hi|
+            j.each_with_index do|jj, wi|
+                cnt += 1 if ii == 0 && jj == 0 && c[hi][wi] == "#"
+            end
+        end
+        ans += 1 if cnt == k
+    end
+end
+puts ans
