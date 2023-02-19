ddf422f71413c42e883f8490e5834f92ed4d3782
廣瀬翔也 <hiroseshouya@hiroseshouyanoMacBook-Pro.local>
2023-02-20 08:56:15 +0900

    23/02/20

--- ruby/C/171.rb
+++ ruby/C/171.rb
@@ -1,2 +1,9 @@
-puts ("a".."z").include?(gets.chomp) ? "a" : "A"
-# "a".."z"のように書いて範囲指定できる
+n = gets.to_i
+arr = ("a".."z").to_a
+ans = []
+while n > 0
+    n -= 1
+    ans = ans.unshift(arr[n % 26])
+    n /= 26
+end
+puts ans.join
