a22f75c124102cea83590cf630ace2462a597057
shoya15 <s.h.rainight@gmail.com>
2023-02-23 04:03:25 +0900

    23/02/23

--- ruby/C/168.rb
+++ ruby/C/168.rb
@@ -1,1 +1,8 @@
-
+# 求める距離をcとする。余弦定理から、c=sqrt(a^2+b^2-2abcosC)
+# 時針は、１時間で30°->１分で0.5°
+# 分針は、１分で6°
+a, b, h, m = gets.split.map(&:to_i)
+hour = 30 * h + 0.5 * m
+minute = 6 * m
+deg = hour - minute
+puts Math.sqrt(a ** 2 + b ** 2 - 2 * a * b * Math.cos(Math::PI * (deg / 180)))
