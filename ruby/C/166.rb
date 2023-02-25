7c14224986597cf7de236e2a927625b47569504e
shoya15 <s.h.rainight@gmail.com>
2023-02-25 15:36:01 +0900

    23/02/25

--- ruby/C/166.rb
+++ ruby/C/166.rb
@@ -1,1 +1,22 @@
-puts gets.chomp == "ABC" ? "ARC" : "ABC"
+n, m = gets.split.map(&:to_i)
+h = gets.split.map(&:to_i)
+arr = Array.new(n){[]}
+m.times do
+    a, b = gets.split.map(&:to_i)
+    a -= 1
+    b -= 1
+    arr[a] << b
+    arr[b] << a
+end
+
+stack = (0..n - 1).to_a
+ans = 0
+while node = stack.pop
+    flg = true
+    arr[node].each do|i|
+        flg = false if h[node] <= h[i]
+    end
+    next if !flg
+    ans += 1
+end
+puts ans
