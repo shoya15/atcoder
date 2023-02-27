6554543ddfff993deeb89f3c6f681452fd1e3494
shoya15 <s.h.rainight@gmail.com>
2023-02-27 17:35:18 +0900

    23/02/27

--- ruby/C/164.rb
+++ ruby/C/164.rb
@@ -1,2 +1,10 @@
-s, w = gets.split.map(&:to_i)
-puts s <= w ? "unsafe" : "safe"
+n = gets.to_i
+hash = {}
+ans = 0
+n.times do
+    s = gets.chomp
+    next if hash[s]
+    hash[s] = true
+    ans += 1
+end
+puts ans
