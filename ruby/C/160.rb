23de4d2d01f7087402e67227529c068a7bba9e7b
shoya15 <s.h.rainight@gmail.com>
2023-03-03 06:59:19 +0900

    23/03/03

--- ruby/C/160.rb
+++ ruby/C/160.rb
@@ -1,2 +1,10 @@
-s = gets.chomp
-puts s[2] == s[3] && s[4] == s[5] ? "Yes" : "No"
+k, n = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i)
+
+ans = a[n - 1] - a[0]
+for i in 1..n - 1
+    start = k - a[i]
+    goal = a[i - 1]
+    ans = [ans, start + goal].min
+end
+puts ans
