609b2726b5a8e48101d49c08bdee4ebd38c8ff33
shoya15 <s.h.rainight@gmail.com>
2023-03-05 09:01:57 +0900

    23/03/05

--- ruby/C/158.rb
+++ ruby/C/158.rb
@@ -1,2 +1,10 @@
-s = gets.chomp
-puts s[0] == s[1] && s[1] == s[2] ? "No" : "Yes"
+a, b = gets.split.map(&:to_i)
+for i in 1..10000
+    aa = (i * 8) / 100
+    bb = (i * 10) / 100
+    if aa == a && bb == b
+        puts i
+        exit
+    end
+end
+puts -1
