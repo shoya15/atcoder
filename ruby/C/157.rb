6e9c832273efedaa4106a66706d1fde04730221b
shoya15 <s.h.rainight@gmail.com>
2023-03-06 09:26:09 +0900

    23/03/06

--- ruby/C/157.rb
+++ ruby/C/157.rb
@@ -1,2 +1,22 @@
-n = gets.to_i
-p n.even? ? n / 2 : n / 2 + 1
+n, m = gets.split.map(&:to_i)
+
+arr = []
+m.times do
+    arr << gets.split.map(&:to_i)
+end
+
+for i in 0..999
+    str = i.to_s
+    next if str.size != n
+    flg = true
+    m.times do|j|
+        n.times do|k|
+            flg = false if k == arr[j][0] - 1 && str[k] != arr[j][1].to_s
+        end
+    end
+    if flg
+        puts i
+        exit
+    end
+end
+puts -1
