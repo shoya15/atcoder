544acfc4ae3aaed751416a0d815b6bb44c87ba28
shoya15 <s.h.rainight@gmail.com>
2023-03-07 11:35:18 +0900

    23/03/07

--- ruby/C/156.rb
+++ ruby/C/156.rb
@@ -1,2 +1,11 @@
-n, r = gets.split.map(&:to_i)
-p n >= 10 ? r : r + 100 * (10 - n)
+n = gets.to_i
+x = gets.split.map(&:to_i)
+ans = 10 ** 18
+for i in 1..100
+    sum = 0
+    n.times do|j|
+        sum += (x[j] - i) ** 2
+    end
+    ans = [ans, sum].min
+end
+puts ans
