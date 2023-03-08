d613c7fe5134d5fa42d4b7f1db3c98dce1d809d3
shoya15 <s.h.rainight@gmail.com>
2023-03-08 12:03:22 +0900

    23/03/08

--- ruby/C/155.rb
+++ ruby/C/155.rb
@@ -1,2 +1,12 @@
-a, b, c = gets.split.map(&:to_i).sort
-puts a == b && b != c || a != b && b == c ? "Yes" : "No"
+n = gets.to_i
+hash = Hash.new(0)
+n.times do
+    s = gets.chomp
+    hash[s] += 1
+end
+
+max = hash.values.max
+hash = hash.sort
+hash.each do|hash|
+    puts hash[0] if hash[1] == max
+end
