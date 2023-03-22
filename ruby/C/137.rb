0f8350f476f5067a43965cd18cef77d35a3d4b53
shoya15 <s.h.rainight@gmail.com>
2023-03-22 23:23:16 +0900

    23/03/22

--- ruby/C/137.rb
+++ ruby/C/137.rb
@@ -1,4 +1,9 @@
-# 整数 A, B があります。
-# A+B,  A−B,  A×B の中で最大の数を出力してください。
-a, b = gets.split.map(&:to_i)
-p [a + b, a - b, a * b].max
+n = gets.to_i
+ans = 0
+hash = Hash.new(0)
+n.times do
+    s = gets.chomp.chars.sort
+    ans += hash[s]
+    hash[s] += 1
+end
+puts ans
