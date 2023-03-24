91200b62e93f00a6e5788aa40a570655331de0f3
shoya15 <s.h.rainight@gmail.com>
2023-03-24 21:22:49 +0900

    23/03/24

--- ruby/C/129.rb
+++ ruby/C/129.rb
@@ -1,7 +1,19 @@
-# 空港 A, B, C があり、それぞれの空港の間では、双方向に飛行機が運航しています。
-# 空港 A, B 間の飛行時間は片道 P 時間、空港 B, C 間の飛行時間は片道 Q 時間、空港 C, A 間の飛行時間は片道 R 時間です。
-# いずれかの空港からスタートして他の空港に飛行機で移動し、さらにそのどちらでもない空港に飛行機で移動するような経路を考えます。
-# 飛行時間の和は最短で何時間になるでしょうか。
-P, q, r = gets.split.map(&:to_i).sort
-p P + q
-# 02'00
+n, m = gets.split.map(&:to_i)
+hash = {}
+m.times do|i|
+    a = gets.to_i
+    hash[a] = true
+end
+
+mod = 1000000007
+dp = Array.new(n + 1, 0)
+dp[0] = 1
+for i in 1..n
+    next if hash[i]
+    if i == 1
+        dp[1] += dp[0]
+    else
+        dp[i] = (dp[i - 1] + dp[i - 2]) % mod
+    end
+end
+puts dp[n]
