d9672490c23ef23c1a20e4b59d617aa360575f15
shoya15 <s.h.rainight@gmail.com>
2023-03-26 09:51:42 +0900

    23/03/26

--- ruby/B/295.rb
+++ ruby/B/295.rb
@@ -1,5 +1,28 @@
 r, c = gets.split.map(&:to_i)
-arr = []
+b = []
 r.times do
-    arr << gets.split(" ")
+    b << gets.chomp
+end
+
+arr = Array.new(r){Array.new(c, false)}
+r.times do|i|
+    c.times do|j|
+        next if b[i][j] == "#" || b[i][j] == "."
+        d = b[i][j].to_i
+        r.times do|k|
+            c.times do|l|
+                arr[k][l] = true if (i - k).abs + (j - l).abs <= d
+            end
+        end
+    end
+end
+
+r.times do|i|
+    c.times do|j|
+        b[i][j] = "." if arr[i][j]
+    end
+end
+
+r.times do|i|
+    puts b[i]
 end
