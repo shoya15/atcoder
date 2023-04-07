ec810751b2bcceb357f607f47b827e787c3d31b8
shoya15 <s.h.rainight@gmail.com>
2023-04-07 15:24:09 +0900

    23/04/07

--- ruby/C/287.rb
+++ ruby/C/287.rb
@@ -6,23 +6,34 @@ m.times do
     array[v] << u
 end
 
+if m != n - 1
+    puts "No"
+    exit
+end
+
+for i in 1..n
+    if array[i].size > 2
+        puts "No"
+        exit
+    end
+end
+
 searched = Array.new(n + 1, false)
 searched[1] = true
 stack = [1]
-
-count, connect = 0, 0
-for i in 1..n
-    next if searched[i]
-    connect += 1
-    while node = stack.pop
-        array[node].each do|i|
-            if searched[i]
-                count += 1
-                next 
-            end
-            searched[i] = true
-            stack << i
+while node = stack.pop
+    array[node].each do|factor|
+        if !searched[factor]
+            searched[factor] = true
+            stack << factor
         end
     end
 end
-puts count == n - 1 && connect == 1 ? "Yes" : "No"
+
+for i in 1..n
+    if !searched[i]
+        puts "No"
+        exit
+    end
+end
+puts "Yes"
