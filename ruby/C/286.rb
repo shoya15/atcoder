05bbc3be1b998413a752cf60bc2459bb24913960
shoya15 <s.h.rainight@gmail.com>
2023-04-08 20:40:44 +0900

    23/04/08

--- ruby/C/286.rb
+++ ruby/C/286.rb
@@ -1,13 +1,9 @@
 n, a, b = gets.split.map(&:to_i)
 s = gets.chomp.chars * 2
-ans = 10 ** 18
+ans = Float::INFINITY
 n.times do|i|
-    sum = a * i
-    (n / 2).times do|j|
-        l = i + j
-        r = i + n - j - 1
-        sum += b if s[l] != s[r]
-    end
+    count = (0...n / 2).count{ |j| s[i + j] != s[i + n - j - 1] }
+    sum = (a * i) + (b * count)
     ans = [ans, sum].min
 end
 puts ans
