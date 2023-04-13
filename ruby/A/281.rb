c22d1d0c0f9cf48286dd24d21825be213133d0aa
shoya15 <s.h.rainight@gmail.com>
2023-04-14 08:18:53 +0900

    update

--- ruby/A/281.rb
+++ ruby/A/281.rb
@@ -1,3 +1,3 @@
 n = gets.to_i
-arr = (0..n).to_a.reverse
-puts arr
+array = *(0..n)
+puts array.reverse
--- ruby/B/281.rb
+++ ruby/B/281.rb
@@ -1,16 +1,17 @@
 s = gets.chomp
-arr = ('A'..'Z').to_a
 n = s[1..-2].to_i
 if s.size != 8
-  puts 'No'
+  puts "No"
 else
-  if !arr.include?(s[0]) || !arr.include?(s[-1])
-    puts 'No'
+  array = *"A".."Z"
+  if !array.include?(s[0]) || !array.include?(s[-1])
+    puts "No"
     exit
   end
+  
   if 100_000 <= n && n <= 999_999
-    puts 'Yes'
+    puts "Yes"
   else
-    puts 'No'
+    puts "No"
   end
 end
