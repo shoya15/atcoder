712853a8a0680c0bb550e2751bf4344b625c10cf
shoya15 <s.h.rainight@gmail.com>
2023-04-14 14:14:28 +0900

    update

--- ruby/C/282.rb
+++ ruby/C/282.rb
@@ -1,12 +1,24 @@
 n = gets.to_i
 s = gets.chomp
-flag = false
 
-n.times do |i|
-  if s[i] == '"'
-    flag = !flag
-    next
-  end
-  s[i] = '.' if s[i] == ',' && !flag
-end
-puts s
+puts s.gsub(/([^"]+)|".*?"/){ |i| i.include?('"') ? i : i.gsub(/,/, ".") }
+# flag = false
+
+# n.times do |i|
+  # if s[i] == '"'
+  #   flag = !flag
+  #   next
+  # end
+  # s[i] = '.' if s[i] == ',' && !flag
+
+  # next if (s[i] == '"')...(s[i] == '"')
+  # s[i] = "." if s[i] == ","
+# end
+# puts s
+
+# *?:0回以上の繰り返し
+# [＾]:どの文字にもマッチしない
+# +:1回以上の繰り返し
+# ｜：どれかに当てはまる
+# (): キャプチャ（文字を記憶する役割がある）
+# .:任意の文字?
