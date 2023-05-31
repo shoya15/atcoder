bf89a9ab458db701086a2f8b7f90e45aa1b4ab76
shoya15 <s.h.rainight@gmail.com>
2023-05-31 22:53:45 +0900

    update

--- ruby/B/252.rb
+++ ruby/B/252.rb
@@ -1,9 +1,6 @@
-n, k = gets.split.map(&:to_i)
+gets.split
 a = gets.split.map(&:to_i)
 b = gets.split.map(&:to_i)
 
-taste_max = a.max
-array = a.select.with_index.map{ |taste, index| index + 1 if taste == taste_max}
-
-result = b.any?{ |dislike| array.include?(dislike) }
-puts result ? "Yes" : "No"
+array = a.select.with_index.map{ |taste, index| index + 1 if taste == a.max}
+puts b.any?{ |dislike| array.include?(dislike) } ? "Yes" : "No"
