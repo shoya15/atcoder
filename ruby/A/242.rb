1ff8040148ef55379737cca18154730467ec8db1
shoya15 <s.h.rainight@gmail.com>
2023-06-15 15:32:26 +0900

    update

--- ruby/A/242.rb
+++ ruby/A/242.rb
@@ -1,2 +1,2 @@
 a, b, c, x = gets.split.map(&:to_f)
-p x <= a ? 1.000000 : x <= b ? c / (b - a) : 0.000000
+puts x <= a ? 1.000000 : x <= b ? c / (b - a) : 0.000000
--- ruby/C/242.rb
+++ ruby/C/242.rb
@@ -1,11 +1,11 @@
 n = gets.to_i
-mod = 998_244_353
+
+mod = 998244353
 dp = Array.new(n + 1) { Array.new(11, 0) }
-for i in 1..9
-  dp[1][i] = 1
-end
-for i in 1..n - 1
-  for j in 1..9
+[*1..9].each{ dp[1][_1] = 1}
+
+1.upto(n - 1) do |i|
+  1.upto(9) do |j|
     dp[i + 1][j] = (dp[i][j - 1] + dp[i][j] + dp[i][j + 1]) % mod
   end
 end
