d4a214d8504982f81f36343566002b4864b36612
shoya15 <s.h.rainight@gmail.com>
2023-06-28 10:20:18 +0900

    update

--- ruby/B/238.rb
+++ ruby/B/238.rb
@@ -1,14 +1,14 @@
-n = gets.to_i
+gets
 a = gets.split.map(&:to_i)
-arr = []
+
+array = [360]
 deg = 0
-for i in 0..n - 1
-  deg += a[i]
-  arr << deg % 360
+a.each do
+  deg += _1
+  array << deg % 360
 end
-arr = arr.sort
-max = arr[0]
-for i in 1..n - 2
-  max = [max, arr[i + 1] - arr[i]].max
-end
-p [max, 360 - arr[n - 1]].max
+
+array.sort!
+ans = array.first
+array.each_cons(2){ ans = [ans, _2 - _1].max}
+puts ans
